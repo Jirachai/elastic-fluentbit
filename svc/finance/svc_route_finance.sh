09b6a407dc54d824b07dda2cbec9979c6e17ce0b
Jirachai2 <jirachai.porsche@gmail.com>
2025-03-13 22:59:47 +0700

    add finance

--- svc/finance/svc_route_finance.sh
+++ svc/finance/svc_route_finance.sh
@@ -8,6 +8,7 @@ WORK_SPACE="Finance"
 # Service details
 SERVICE_NAME="svc_finance"
 SERVICE_URL="http://thd-liberty.th.msig.com:10002"
+SERVICE_TAGS='["uat", "finance"]'
 
 # Create the service
 echo "Creating service: $SERVICE_NAME..."
@@ -16,13 +17,15 @@ curl -s -X POST "$KONG_ADMIN/$WORK_SPACE/services" \
      -H "Content-Type: application/json" \
      -d '{
            "name": "'"$SERVICE_NAME"'",
-           "url": "'"$SERVICE_URL"'"
+           "url": "'"$SERVICE_URL"'",
+	   "tags": '"$SERVICE_TAGS"'
          }' | jq .
 
 # Function to create a route
 create_route() {
   ROUTE_NAME=$1
   ROUTE_PATH=$2
+  ROUTE_TAGS='["uat", "finance"]'
 
   echo "Creating route: $ROUTE_NAME with path $ROUTE_PATH..."
   curl -s -X POST "$KONG_ADMIN/$WORK_SPACE/routes" \
@@ -33,7 +36,8 @@ create_route() {
              "paths": ["'"$ROUTE_PATH"'"],
              "methods": ["GET", "POST"],
              "strip_path": false,
-             "service": { "name": "'"$SERVICE_NAME"'" }
+             "service": { "name": "'"$SERVICE_NAME"'" },
+	     "tags": '"$ROUTE_TAGS"'
            }' | jq .
 }
 
